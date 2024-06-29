a2f2d612637bde596aa555d365df2a5a373f7f25
noeltiju <noelab04@gmail.com>
2024-06-29 20:03:02 +0530

    Used Llama finetuned for summarization

--- roamify_extension/backend/app/utils/llama_processing.py
+++ roamify_extension/backend/app/utils/llama_processing.py
@@ -1,11 +1,48 @@
-from transformers import AutoModel, AutoTokenizer
+from unsloth import FastLanguageModel
+import torch
+from transformers import TextStreamer
 
-model_name = "Roamify/llama-3-finetuned-model"
-model = AutoModel.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-input_text = "summarize: The quick brown fox jumps over the lazy dog."
-inputs = tokenizer(input_text, return_tensors="pt")
-outputs = model(**inputs)
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
-print(outputs)
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+class LlamaProcessing:
+    def __init__(self, model_path):
+        max_seq_length = 2048
+        dtype = None
+        load_in_4bit = True
+
+        access_token = "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"
+        self.model, self.tokenizer = FastLanguageModel.from_pretrained(
+            model_name = "RoamifyRedefined/finetuned-summarization-llama3",
+            max_seq_length = max_seq_length,
+            dtype = dtype,
+            load_in_4bit = load_in_4bit,
+            token = access_token,
+            # device_map = "auto" # Optionally, let Transformers automatically decide the device mapping
+        )
+
+    def predict_summary(self, text):
+        inputs = self.tokenizer(
+        [
+            alpaca_prompt.format(
+                "Summarize the following text briefly starting with the name of the attraction.", # instruction
+                text, # input
+                "", # output - leave this blank for generation!
+            )
+        ], return_tensors = "pt").to("cuda")
+        text_streamer = TextStreamer(self.tokenizer)
+        result = self.model.generate(**inputs, streamer = text_streamer, max_new_tokens = 128)
+        return result
+
+
+LlamaProcessing = LlamaProcessing("RoamifyRedefined/finetuned-summarization-llama3")
+LlamaProcessing.predict_summary("The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is named after the engineer Gustave Eiffel, whose company designed and built the tower.")
