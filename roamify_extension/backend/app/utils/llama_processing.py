11da79ea1a2a2b07254ed241985caad4503f2ffd
noeltiju <noelab04@gmail.com>
2024-06-29 20:41:17 +0530

    Updated llama-processing.py

--- roamify_extension/backend/app/utils/llama_processing.py
+++ roamify_extension/backend/app/utils/llama_processing.py
@@ -30,6 +30,21 @@ class LlamaProcessing:
             # device_map = "auto" # Optionally, let Transformers automatically decide the device mapping
         )
 
+        self.model = FastLanguageModel.get_peft_model(
+            self.model,
+            r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+            target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                            "gate_proj", "up_proj", "down_proj",],
+            lora_alpha = 16,
+            lora_dropout = 0, # Supports any, but = 0 is optimized
+            bias = "none",    # Supports any, but = "none" is optimized
+            # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+            use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+            random_state = 3407,
+            use_rslora = False,  # We support rank stabilized LoRA
+            loftq_config = None, # And LoftQ
+        )
+
     def predict_summary(self, text):
         inputs = self.tokenizer(
         [
